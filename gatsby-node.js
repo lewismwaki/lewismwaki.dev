b64b0491e62fc83dc675cb412eb82b89b891135e
lewymwaki <mwakicodes@gmail.com>
2021-11-02 22:38:32 +0300

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,33 @@
+// Common assets file types which are for `file-loader`
+const reImage = /\.(ico|jpg|jpeg|png|gif|eot|otf|svg|webp|ttf|woff|woff2)$/;
+// css files
+const reScss = /\.(css|scss|sass|less)$/;
+
 exports.onCreateWebpackConfig = ({
-    stage,
-    rules,
-    loaders,
-    plugins,
-    actions,
-  }) => {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-            {
-                test: /\.(s*)css$/,
-                use: [
-                  'style-loader',
-                  'css-loader',
-                ]
-              },
-        ],
-      },
-      plugins: [
-        plugins.define({
-          __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
-        }),
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    externals: [
+      nodeExternals({
+        allowlist: [reScss],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(s*)css$/,
+          use: ["style-loader", "css-loader"],
+        },
       ],
-    })
-  }
+    },
+    plugins: [
+      plugins.define({
+        __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
+      }),
+    ],
+  });
+};
